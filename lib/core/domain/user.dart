b52a31205a72aeb3ed36018350818b35d8a2da48
Renan Almeida <renan_lopes_almeida@hotmail.com>
2021-06-06 09:59:49 -0300

    adding JsonSerializable into user model

--- lib/core/domain/user.dart
+++ lib/core/domain/user.dart
@@ -1,7 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+//part 'user.g.dart';
+
+@JsonSerializable(explicitToJson: true)
 class User {
-  String name;
-  String email;
-  String password;
-  String confirmedPassword;
-  String token;
+  const User({
+    @required this.name,
+    @required this.email,
+    @required this.password,
+    @required this.confirmedPassword,
+    @required this.token,
+    @required this.id,
+  });
+
+  @JsonKey(name: 'id')
+  final String id;
+  @JsonKey(name: 'name')
+  final String name;
+  @JsonKey(name: 'email')
+  final String email;
+  @JsonKey(name: 'password')
+  final String password;
+  @JsonKey(name: 'confirmedPassword')
+  final String confirmedPassword;
+  @JsonKey(name: 'token')
+  final String token;
 }
