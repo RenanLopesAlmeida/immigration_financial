7aff3257c2d15f810b649c43eb7088e0292ce9b6
Renan Almeida <renan_lopes_almeida@hotmail.com>
2021-06-26 10:32:20 -0300

    removing unnecessary code

--- lib/modules/login/ui/components/sign_up_button.dart
+++ lib/modules/login/ui/components/sign_up_button.dart
@@ -3,30 +3,20 @@ part of '../login_page.dart';
 class _SignUpButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final presenter = Provider.of<LoginPresenter>(context, listen: false);
-
     return Container(
       margin: EdgeInsets.only(
         top: Spacing.small,
         bottom: Spacing.normal,
       ),
       height: 40,
-      child: StreamBuilder<bool?>(
-        stream: presenter.isFormValidStream,
-        builder: (_, snapshot) {
-          bool? isFormValid = snapshot.data;
-
-          return TextButton(
-            onPressed:
-                null, //isFormValid == false ? null : presenter.authenticate,
-            child: Text(
-              'Sign Up',
-              style: Theme.of(context).textTheme.button!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-          );
-        },
+      child: TextButton(
+        onPressed: null, //isFormValid == false ? null : presenter.authenticate,
+        child: Text(
+          'Sign Up',
+          style: Theme.of(context).textTheme.button!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
